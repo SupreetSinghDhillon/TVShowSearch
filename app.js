ca7ed69fc08b0506672b2773194d799651e92dd0
Supreet <ssd19@sfu.ca>
2023-09-18 08:58:50 -0700

    adding description below every show

--- app.js
+++ app.js
@@ -3,17 +3,23 @@ form.addEventListener("submit", async function (e) {
   e.preventDefault();
   const searchTerm = form.elements.query.value;
   const config = { params: { q: searchTerm } };
-  const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
-  makeImages(res.data);
-  form.elements.query.value = "";
+  try {
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+    makeImagesAndText(res.data);
+  } catch (e) {
+    console.log("Error", e);
+  }
 });
 
-const makeImages = (shows) => {
+const makeImagesAndText = (shows) => {
   for (let result of shows) {
     if (result.show.image) {
       const img = document.createElement("IMG");
       img.src = result.show.image.medium;
       document.body.append(img);
+      const text = document.createElement("P");
+      text.innerText = result.show.summary;
+      document.body.append(text);
     }
   }
 };
